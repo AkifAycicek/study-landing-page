19fbffdc0605a9df48dc201b793cf83fd37266cc
akif ayçiçek <akifaycicek@gmail.com>
2024-07-28 12:58:45 +0300

    fixes for gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,15 @@ import { setupi18N } from "./plugins/i18n.js";
 // Comment trick for disable auto organize import
 import "@style/main.scss";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
 
 Promise.allSettled([loadFonts(), setupi18N()]).then(async () => {
   reactRoot.render(
-    <BrowserRouter basename="/study-landing-page">
+    <HashRouter>
       <App />
-    </BrowserRouter>,
+    </HashRouter>,
   );
 });
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "study-landing-page",
+  base: "study-landing-page/#/",
   plugins: [
     react(),
     eslint({
