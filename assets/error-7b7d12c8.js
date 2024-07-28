2e486d2bd9e494c707e67dc2884ba56521bef7dc
akif ayçiçek <akifaycicek@gmail.com>
2024-07-28 14:57:43 +0300

    Updates

---
+++ assets/error-7b7d12c8.js
@@ -0,0 +1,1 @@
+const o="Something went wrong",e="Home Page",n={404:"Not Found",something_went_wrong:o,home_page:e};export{n as default,e as home_page,o as something_went_wrong};
---
+++ assets/error-de8415ad.js
@@ -0,0 +1,1 @@
+const e="Bir şeyler yanlış gitti",n="Anasayfa",o={404:"Bulunamadı",something_went_wrong:e,home_page:n};export{o as default,n as home_page,e as something_went_wrong};
