6a0c11e7d8abe062ee77c9fbf7ba1b10fa1aca4f
akif ayçiçek <akifaycicek@gmail.com>
2024-07-28 20:07:49 +0300

    Updates

--- assets/error-7b7d12c8.js
+++ assets/error-7b7d12c8.js
@@ -1,1 +1,2 @@
 const o="Something went wrong",e="Home Page",n={404:"Not Found",something_went_wrong:o,home_page:e};export{n as default,e as home_page,o as something_went_wrong};
+//# sourceMappingURL=error-7b7d12c8.js.map
--- assets/error-de8415ad.js
+++ assets/error-de8415ad.js
@@ -1,1 +1,2 @@
 const e="Bir şeyler yanlış gitti",n="Anasayfa",o={404:"Bulunamadı",something_went_wrong:e,home_page:n};export{o as default,n as home_page,e as something_went_wrong};
+//# sourceMappingURL=error-de8415ad.js.map
